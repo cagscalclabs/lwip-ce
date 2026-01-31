dc9f9a07aed298226eb7a94bd8c4caced142f96d
Anthony Cagliano <admin@cagstech.com>
2026-01-31 13:57:16 -0500

    push fix to cryptoguard

--- src/tls/core/share/crypto_guard.asm
+++ src/tls/core/share/crypto_guard.asm
@@ -9,132 +9,66 @@ include "virtuals.inc"
 assume adl=1
 section .text
 
-public _tls_crypto_guard_start
-public _tls_crypto_guard_stop
+public _tls_crypto_guard_enable
+public _tls_crypto_guard_disable
 
-; bool tls_crypto_guard_start(void)
-_tls_crypto_guard_start:
-	ld a, i
-	ld hl, 0
-	add hl, sp
-	call _cg_push_state
-	ret
+?stackBot		:= 0D1987Eh
 
-; bool tls_crypto_guard_stop(void)
-_tls_crypto_guard_stop:
-
-	call _cg_pop_state
-	or a
-	ret z		; return if failed
-
-	; wipe from current return address (sp + 3) up to saved SP - 1
-	ld hl, 3
-	add hl, sp
-	ex de, hl
-	ld hl, (_cg_saved_sp)
-	dec hl
-	or a
-	sbc hl, de
-	jr c, .no_wipe
-	push hl
-	pop bc
-	ld hl, (_cg_saved_sp)
-	dec hl
-	ld (hl), 0
-	jr z, .no_wipe
-	dec bc
-	push hl
-	pop de
-	dec de
-	lddr
-.no_wipe:
-.done:
-	ret
-
-_erase_saved_a: db 0
-_erase_saved_e: db 0
-_erase_saved_hl: db 0, 0, 0
-
-_cg_push_state:
-; push state, A = interrupt state, HL = SP
-; returns A = success/fail
-	di
-	push hl
-		push af
-			; test if we have a stack pointer
-			ld hl, (_crypto_guard_stack_ptr)
-			ld de, 0
-			add hl, de
-			or a
-			sbc hl, de
-			jr nz, .not_zero
-			ld hl, _crypto_guard_stack_start
-			ld (_crypto_guard_stack_ptr), hl	; set the starting pointer
-			jr .skip_stack_end_test
-		.not_zero:
-			; test if we're at the stack end
-			ld bc, (_crypto_guard_stack_ptr)
-			ld hl, _crypto_guard_stack_end
-			or a
-			sbc hl, bc
-			jr z, .exit_no_push
-		.skip_stack_end_test:
-		pop af
-		; save interrupt state
-		ld hl, (_crypto_guard_stack_ptr)
-		ld (hl), a
-	pop de
-	inc hl
-	; save stack pointer
-	ld (hl), de
-	inc hl
-	inc hl
-	inc hl
-	ld (_crypto_guard_stack_ptr), hl
-	ld a, 1
-	ret
-.exit_no_push
-		pop af
-	pop hl
-	ld a, 0
-	ret
-
-
-_cg_pop_state:
-; pops last state off the stack
-; returns A = success/fail, HL = previous SP
-	; test if we have a stack pointer
-	ld hl, (_crypto_guard_stack_ptr)
-	ld de, 0
-	add hl, de
-	or a
-	sbc hl, de
-	jr z, .not_zero
-	ld hl, (_crypto_guard_stack_ptr)
-	ld bc, _crypto_guard_stack_start
+; void tls_crypto_guard_enable(void)
+_tls_crypto_guard_enable:
+	ld hl, (_crypto_guard_state_ptr)
+	ld a, h
+	or l
+	jr nz, .has_ptr
+; .no_has_ptr
+	ld hl, _crypto_guard_state_start
+	jr .save_state
+.has_ptr:
+	ld bc, _crypto_guard_state_end
 	or a
 	sbc hl, bc
-	jr z, .no_state
-	ld hl, (_crypto_guard_stack_ptr)
-	dec hl
-	dec hl
-	dec hl
+	ld hl, (_crypto_guard_state_ptr)
+	ret z
+.save_state:
+	ld a, i
+	ld (hl), a
+	inc hl
+	ld (_crypto_guard_state_ptr), hl
+	di
+	ret
+
+; void tls_crypto_guard_disable(void)
+_tls_crypto_guard_disable:
+; load sp into hl, store 0 to data at hl, decrement hl, store hl to de, subtract stack bottom to store into bc, lddr
+; set hl to SP - 1: scf \ sbc hl, hl \ add hl, sp
+; _erase_stack:
+	; set from stackBot + 4 to ix - 1 to 0
+	lea de, ix - 2
+	ld hl, -(stackBot + 3)
+	add hl, de
 	push hl
-		dec hl
-		ld (_crypto_guard_stack_ptr), hl
-		ld a, (hl)
-	pop hl
-	ld hl, (hl)
+	pop bc
+	lea hl, ix - 1
+	ld (hl), 0
+	lddr
+
+	; restore interrupts
+	; do we have the pointer
+	ld hl, (_crypto_guard_state_ptr)
+	ld a, h
+	or l
+	ret z
+	; is the pointer at the start
+	ld bc, _crypto_guard_state_start
 	or a
-	jr z, .no_interrupt_enable
+	sbc hl, bc
+	ret z
+	ld hl, (_crypto_guard_state_ptr)
+	dec hl
+	ld (_crypto_guard_state_ptr), hl
+	ld a, (hl)
+	or a
+	ret z
 	ei
-.no_interrupt_enable:
-	ld a, 1
-	ret
-.no_state:
-	ld a, 0
 	ret
 
-
-
-_cg_stack_inc		:= 4
